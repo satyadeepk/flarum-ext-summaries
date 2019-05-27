dd2d6ecbcb84a4a2b6d4366accb63ffe727f9baa
Satyadeep <satyadeep.1991@gmail.com>
2019-05-27 09:54:47 +0530

    Extracting and showing first image of a post in summary

--- js/src/forum/addSummaryExcerpt.js
+++ js/src/forum/addSummaryExcerpt.js
@@ -5,21 +5,29 @@ import DiscussionListItem from 'flarum/components/DiscussionListItem';
 import { truncate } from 'flarum/utils/string';
 
 export default function addSummaryExcerpt() {
-  extend(DiscussionList.prototype, 'requestParams', function(params) {
-    params.include.push('firstPost');
-  });
+    extend(DiscussionList.prototype, 'requestParams', function(params) {
+        params.include.push('firstPost');
+    });
 
-  extend(DiscussionListItem.prototype, 'infoItems', function(items) {
-    const discussion = this.props.discussion;
+    extend(DiscussionListItem.prototype, 'infoItems', function(items) {
+        const discussion = this.props.discussion;
 
-    const firstPost = discussion.firstPost();
-    const excerptLength = app.forum.attribute('flarum-ext-summaries.excerpt_length') || 200;
+        const firstPost = discussion.firstPost();
+        const excerptLength =
+            app.forum.attribute('flarum-ext-summaries.excerpt_length') || 200;
 
-    if (firstPost) {
-//       const excerpt = <span>{truncate(firstPost.contentPlain(), excerptLength)}</span>;
-      const excerpt = <span>{truncate(firstPost.contentHtml(), excerptLength)}</span>;
+        if (firstPost) {
+            const firstPostHtml = firstPost.contentHtml();
+            const reg = /img src="(.*?)"/;
 
-      items.add('excerpt', excerpt, -100);
-    }
-  });
+            const match = firstPostHtml && firstPostHtml.match(reg);
+            const firstImageUrl = match && match[1];
+            console.log(firstPostHtml, firstImageUrl);
+            //       const excerpt = <span>{truncate(firstPost.contentPlain(), excerptLength)}</span>;
+            if (firstImageUrl) {
+                const excerpt = <img src={firstImageUrl} width="200" />;
+                items.add('excerpt', excerpt, -100);
+            }
+        }
+    });
 }
